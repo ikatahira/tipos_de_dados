9ca65f26dbb30e7a3c7f972762abf333856f80ed
ikatahira <isaque.katahira@fatec.sp.gov.br>
2023-02-07 15:17:46 +0000

    salvando

--- Variaveis.java
+++ Variaveis.java
@@ -1,3 +1,14 @@
 public class Variaveis{
+    public static void main(String[] args) {
+        
     
-}
+  int idade=39;
+  byte quantidadeBolas=7;
+  char letra='M';
+  boolean vivo=false;
+  double salario = 1302.00;
+  float salarioMenor =1212.00f;
+  short alunosPalmital = 10000;
+  long populacaoPalmital = 22000;
+  System.out.println(idade);
+}}
