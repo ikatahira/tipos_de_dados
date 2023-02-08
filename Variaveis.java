9d7bea5210d8e07dae9454852708c2b93c802475
ikatahira <isaque.katahira@fatec.sp.gov.br>
2023-02-08 17:04:10 +0000

    variaveis salvas

--- Variaveis.java
+++ Variaveis.java
@@ -2,13 +2,17 @@ public class Variaveis{
     public static void main(String[] args) {
         
     
-  int idade=39;
+  int  idade=39 ;
   byte quantidadeBolas=7;
   char letra='M';
   boolean vivo=false;
   double salario = 1302.00;
   float salarioMenor =1212.00f;
   short alunosPalmital = 10000;
-  long populacaoPalmital = 22000;
-  System.out.println(idade);
+            long populacaoPalmital = 22000;
+  String nome;
+  nome="Netto da Etec Palmital";
+  System.out.println("Meu nome é: "+nome + " tenho: "+idade+" anos");
+  System.out.println(nome);
+  System.out.println(nome);
 }}
