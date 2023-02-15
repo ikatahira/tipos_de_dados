aea618006fc985fa225e5fbc86cfdc0305abb109
ikatahira <isaque.katahira@fatec.sp.gov.br>
2023-02-15 17:25:06 +0000

    fatorial

--- EstruturaDeRepeticao.java
+++ EstruturaDeRepeticao.java
@@ -1,4 +1,19 @@
 public class EstruturaDeRepeticao {
     
+
+    public static void main(String args[]){
+        int fatResult = 1;
+        for(int i=0;i>1;i--){
+            if(i ==0){
+                fatResult = 1;
+                break;
+            }else{
+                fatResult = i * fatResult;
+            }
+        }
+
+        System.out.println(fatResult);
+
+    }
     
 }
